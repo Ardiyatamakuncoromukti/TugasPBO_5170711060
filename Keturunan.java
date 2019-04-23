7ceda4c569c86fcb4f6bbb9c1e2231b18a16f381
Ardiyatamakuncoromukti <49850220+Ardiyatamakuncoromukti@users.noreply.github.com>
2019-04-23 13:24:45 +0700

    Create Keturunan.java

---
+++ Keturunan.java
@@ -0,0 +1,25 @@
+package manusia;
+
+
+public class Keturunan {
+  
+    
+    protected void CahLanang(){
+        System.out.println("Cah Lanang");
+        //System.out.println();
+        System.out.println("Ganteng");
+        System.out.println("Tinggi");
+        System.out.println("Maco");
+        System.out.println("Sawo Matang/Kuning Langsat");
+        System.out.println();
+                    }
+    protected void CahWedok(){
+        System.out.println("Cah Wedeok");
+        //System.out.println();
+        System.out.println("Ayu");
+        System.out.println("Lembut");
+        System.out.println("Gemulai");
+        System.out.println("Kuning langsat/putih Susu");
+    }
+    
+}
