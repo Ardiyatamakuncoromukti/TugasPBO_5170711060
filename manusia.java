cd3f65714ccae3e8dd17602284460e7abbfb3b4b
Ardiyatamakuncoromukti <49850220+Ardiyatamakuncoromukti@users.noreply.github.com>
2019-04-23 13:21:47 +0700

    Create manusia.java

---
+++ manusia.java
@@ -0,0 +1,17 @@
+package manusia;
+
+public class Manusia {
+ 
+public static void main(String[]args){   
+
+Ahlak behaviour = new Ahlak();
+Keturunan khas = new Keturunan();
+
+behaviour.Mlaku();
+behaviour.Ndelok();
+behaviour.Madang();
+khas.CahLanang();
+khas.CahWedok();
+
+}   
+}
